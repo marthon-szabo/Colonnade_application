4ac7ae71ef0298257b45811408ada11e317bb1d7
Márton Szabó <makotoimx@gmail.com>
2020-10-13 12:08:01 +0200

    Add Extra class

---
+++ ColonnadeApplication/ExerciseA/Models/Extra.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExerciseA.Models
+{
+    public sealed class Extra : Options
+    {
+        public Extra() : base(2, 150000000, 100000, 100000, 200000000)
+        {
+        }
+    }
+}
