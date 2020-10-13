e5d022e8393a0d8f2bbd923cbfadc08d8206dfd7
Márton Szabó <makotoimx@gmail.com>
2020-10-13 11:38:05 +0200

    Add Options class

--- ColonnadeApplication/ExerciseA/Models/Option.cs
+++ ColonnadeApplication/ExerciseA/Models/Option.cs
@@ -1,6 +1,24 @@
-﻿namespace ExerciseA.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ExerciseA.Models
 {
     public class Option
     {
+        [Key]
+        public int Id { get; set; }
+
+        [NotNull]
+        public string Type { get; set; }
+
+        [NotNull]
+        public int AccidentInsuranceLimit { get; set; }
+
+        [NotNull]
+        public int Cash { get; set; }
+        [NotNull]
+        public int Luggage { get; set; }
+        [NotNull]
+        public string LuggageLimit { get; set; }
     }
 }
---
+++ ColonnadeApplication/ExerciseA/Models/Options.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExerciseA.Models
+{
+    public abstract class Options
+    {
+        private protected abstract int Id { get; set; }
+        private protected abstract string Type { get; set; }
+        private protected abstract int AccidentInsuranceLimit { get; set; }
+        private protected abstract int Cash { get; set; }
+        private protected abstract int Luggage { get; set; }
+        private protected abstract string LuggageLimit { get; set; }
+    }
+}
