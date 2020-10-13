32d3972836ba1b1a83e52d5c10979850eaa2a18e
Márton Szabó <makotoimx@gmail.com>
2020-10-13 14:00:46 +0200

    Implement AddUser()

--- ColonnadeApplication/ExerciseA/Models/Repositories/UserSQLRepository.cs
+++ ColonnadeApplication/ExerciseA/Models/Repositories/UserSQLRepository.cs
@@ -5,8 +5,34 @@ using System.Threading.Tasks;
 
 namespace ExerciseA.Models.Repositories
 {
-    public class UserSQLRepository
+    public class UserSQLRepository : IUserRepository
     {
+        private readonly ColonnadeAppDbContext context;
 
+        public UserSQLRepository(ColonnadeAppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async void AddUser(User newUser)
+        {
+            context.Users.Add(newUser);
+            await context.SaveChangesAsync();
+        }
+
+        public async void DeletUserById(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
