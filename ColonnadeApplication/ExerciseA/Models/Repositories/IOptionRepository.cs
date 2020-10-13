30cd1835779953f0c36966ddbe3de12a4e6d7bbf
Márton Szabó <makotoimx@gmail.com>
2020-10-13 14:06:52 +0200

    Add IOptionRepository interface

---
+++ ColonnadeApplication/ExerciseA/Models/Repositories/IOptionRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExerciseA.Models.Repositories
+{
+    public interface IOptionRepository
+    {
+        public void AddOptionAsync(Options option);
+        public Task<Options> GetOptionByIdAsync(Options option);
+    }
+}
