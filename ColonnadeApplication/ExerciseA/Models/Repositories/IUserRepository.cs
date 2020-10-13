115508af65ccc7ae0357eff58af78c2bcff87b57
Márton Szabó <makotoimx@gmail.com>
2020-10-13 13:55:47 +0200

    Add IUserRepository interface

---
+++ ColonnadeApplication/ExerciseA/Models/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExerciseA.Models
+{
+    public interface IUserRepository
+    {
+        public void AddUser(User newUser);
+        public User GetUserById(int id);
+        public void UpdateUser(User user);
+        public void DeletUserById(int userId);
+    }
+}
