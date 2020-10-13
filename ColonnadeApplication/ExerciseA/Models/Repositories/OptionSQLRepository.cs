99fe2c63cd4a7eeaf1ef725b3078a0bda430629e
Márton Szabó <makotoimx@gmail.com>
2020-10-13 14:09:02 +0200

    Create OptionSQLRepository

---
+++ ColonnadeApplication/ExerciseA/Models/Repositories/OptionSQLRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExerciseA.Models.Repositories
+{
+    public class OptionSQLRepository : IOptionRepository
+    {
+        private readonly ColonnadeAppDbContext context;
+
+        public OptionSQLRepository(ColonnadeAppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void AddOptionAsync(Options option)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Options> GetOptionByIdAsync(Options option)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
