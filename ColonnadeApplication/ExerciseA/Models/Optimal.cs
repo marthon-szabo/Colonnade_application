865da801f7c15f769831df5c416944db893ee0d5
Márton Szabó <makotoimx@gmail.com>
2020-10-13 12:05:40 +0200

    Add Optimal class

---
+++ ColonnadeApplication/ExerciseA/Models/Optimal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExerciseA.Models
+{
+    public sealed class Optimal : Options
+    {
+        public Optimal() 
+            : base (1, 200000000, 100000, 200000, 200000000)
+        {
+        }        
+    }
+}
--- ColonnadeApplication/ExerciseA/Models/Options.cs
+++ ColonnadeApplication/ExerciseA/Models/Options.cs
@@ -7,11 +7,25 @@ namespace ExerciseA.Models
 {
     public abstract class Options
     {
-        private protected abstract int Id { get; set; }
-        private protected abstract string Type { get; set; }
-        private protected abstract int AccidentInsuranceLimit { get; set; }
-        private protected abstract int Cash { get; set; }
-        private protected abstract int Luggage { get; set; }
-        private protected abstract string LuggageLimit { get; set; }
+        private protected readonly int _id;
+        private protected readonly int _accidentalInsuranceLimit;
+        private protected readonly int _cash;
+        private protected readonly int _luggage;
+        private protected readonly int _luggageLimit;
+
+        protected Options(int id, int accidentalInsuranceLimit, int cash, int luggage, int luggageLimit)
+        {
+            _id = id;
+            _accidentalInsuranceLimit = accidentalInsuranceLimit;
+            _cash = cash;
+            _luggage = luggage;
+            _luggageLimit = luggageLimit;
+        }
+
+        public int Id { get => _id;}
+        public int AccidentInsuranceLimit { get => _accidentalInsuranceLimit;}
+        public int Cash { get => _cash;}
+        public int Luggage { get => _luggage;}
+        public int LuggageLimit { get => _luggageLimit;}
     }
 }
