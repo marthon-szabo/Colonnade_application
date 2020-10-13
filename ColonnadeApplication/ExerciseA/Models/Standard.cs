667b934ddf28c987d2a457638d7068ca949f5115
Márton Szabó <makotoimx@gmail.com>
2020-10-13 12:11:31 +0200

    Add Standard class

---
+++ ColonnadeApplication/ExerciseA/Models/Standard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExerciseA.Models
+{
+    public sealed class Standard : Options
+    {
+        public Standard() : base(4, 50000000, 0, 0, 200000000)
+        {
+        }
+    }
+}
