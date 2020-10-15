b381f03c35028da9dce19115638ab749c9a7a08e
Márton Szabó <makotoimx@gmail.com>
2020-10-15 16:16:19 +0200

    Add "Enter" key blocking js into site.js

--- ColonnadeApplication/ExerciseA/wwwroot/js/site.js
+++ ColonnadeApplication/ExerciseA/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const fields = document.querySelectorAll(".form-control");
+
+for (let field of fields) {
+    field.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+            event.preventDefault();
+            console.log(true);
+            return false;
+        };
+    });
+};
