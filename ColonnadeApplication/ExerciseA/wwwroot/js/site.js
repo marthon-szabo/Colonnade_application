131efb4f61471692a362f628dc0fa85a83840bef
Márton Szabó <makotoimx@gmail.com>
2020-10-15 16:18:04 +0200

    Remove unnecessary comments

--- ColonnadeApplication/ExerciseA/wwwroot/js/site.js
+++ ColonnadeApplication/ExerciseA/wwwroot/js/site.js
@@ -1,8 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-const fields = document.querySelectorAll(".form-control");
+﻿const fields = document.querySelectorAll(".form-control");
 
 for (let field of fields) {
     field.addEventListener('keypress', (e) => {
